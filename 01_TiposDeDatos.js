e8c641b6d6ab74969201085049c712fe8ffdf4c7
Carl Alvarez <carlalva410@gmail.com>
2021-11-07 20:16:45 -0300

    Update 01_TiposDeDatos.js

---
+++ 01_TiposDeDatos.js
@@ -0,0 +1,29 @@
+/**
+ * Tipos de datos
+ */
+
+//-numero-number
+console.log(27);
+console.log(27.56);
+
+//-texto--string
+console.log("Carlos Alvarez");
+console.log('Eduardo');
+console.log(`hola, Soy carlos`);
+
+//-valores logicos--booleans
+console.log(true);
+console.log(false);
+
+//-indefinido--undefined
+console.log(undefined);
+
+//null
+console.log(null);
+
+/**
+ * -tipos de datos estructurales
+ */
+// objeto object
+console.log({nombre: 'Carlos', edad: 34, fechaDeNacimiento: null, esAlto: true});
+
